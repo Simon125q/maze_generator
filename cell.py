3116623975474d17de5d6955b9615df49f29aaff
Simon125q <106029626+Simon125q@users.noreply.github.com>
2023-08-01 15:28:26 +0200

    divide into files, add restart option

---
+++ cell.py
@@ -0,0 +1,55 @@
+import pygame
+from settings import *
+
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
+        self.visited = False
+    
+    def draw_current_cell(self, screen):
+        x = self.x * TILE
+        y = self.y * TILE
+        pygame.draw.rect(screen, pygame.Color('saddlebrown'), (x + 2, y + 2, TILE - 2, TILE - 2))
+        
+    def draw(self, screen):
+        x = self.x * TILE
+        y = self.y * TILE
+        
+        if self.visited:
+            pygame.draw.rect(screen, pygame.Color('black'), (x, y, TILE, TILE))
+        
+        if self.walls['top']:
+            pygame.draw.line(screen, pygame.Color('magenta3'), (x, y), (x + TILE, y), 2)
+        if self.walls['right']:
+            pygame.draw.line(screen, pygame.Color('magenta3'), (x + TILE, y), (x + TILE, y + TILE), 2)
+        if self.walls['bottom']:
+            pygame.draw.line(screen, pygame.Color('magenta3'), (x + TILE, y + TILE), (x, y + TILE), 2)
+        if self.walls['left']:
+            pygame.draw.line(screen, pygame.Color('magenta3'), (x, y + TILE), (x, y), 2)
+            
+    def check_cell(self,x, y, grid_cells):
+        find_index = lambda x, y: x + y * COLS
+        if x < 0 or x > COLS - 1 or y < 0 or y > ROWS -1 :
+            return False
+        return grid_cells[find_index(x, y)]
+        
+    def check_neighbors(self, grid_cells):
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1, grid_cells)
+        right = self.check_cell(self.x + 1, self.y, grid_cells)
+        bottom = self.check_cell(self.x, self.y + 1, grid_cells)
+        left = self.check_cell(self.x - 1, self.y, grid_cells)
+        
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+            
+        return choice(neighbors) if neighbors else False
+    
--- maze.py
+++ maze.py
@@ -2,18 +2,17 @@ import pygame
 import sys
 from random import choice
 import csv
-
-RES = WIDTH, HEIGHT = (1202, 902)
-TILE = 50
-COLS = WIDTH // TILE
-ROWS = HEIGHT // TILE
-FPS = 260
+from settings import *
+from cell import Cell
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
+        self.new_maze()
+    
+    def new_maze(self):
         self.grid_cells = [Cell(col, row) for row in range(ROWS) for col in range(COLS)]
         self.current_cell = self.grid_cells[0]
         self.stack = []
@@ -65,8 +64,7 @@ class Game:
                 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    self.loop = False
+                    self.new_maze()
                 if event.key == pygame.K_1:
                     self.print_maze()
                 if event.key == pygame.K_ESCAPE:
@@ -122,66 +120,12 @@ class Game:
                 writer.writerow(row)      
         
     def run(self):
-        while self.loop:
+        while True:
             self.check_events()
             self.draw()
             self.update()
             
-class Cell:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
-        self.visited = False
-    
-    def draw_current_cell(self, screen):
-        x = self.x * TILE
-        y = self.y * TILE
-        pygame.draw.rect(screen, pygame.Color('saddlebrown'), (x + 2, y + 2, TILE - 2, TILE - 2))
-        
-    def draw(self, screen):
-        x = self.x * TILE
-        y = self.y * TILE
-        
-        if self.visited:
-            pygame.draw.rect(screen, pygame.Color('black'), (x, y, TILE, TILE))
-        
-        if self.walls['top']:
-            pygame.draw.line(screen, pygame.Color('magenta3'), (x, y), (x + TILE, y), 2)
-        if self.walls['right']:
-            pygame.draw.line(screen, pygame.Color('magenta3'), (x + TILE, y), (x + TILE, y + TILE), 2)
-        if self.walls['bottom']:
-            pygame.draw.line(screen, pygame.Color('magenta3'), (x + TILE, y + TILE), (x, y + TILE), 2)
-        if self.walls['left']:
-            pygame.draw.line(screen, pygame.Color('magenta3'), (x, y + TILE), (x, y), 2)
-            
-    def check_cell(self,x, y, grid_cells):
-        find_index = lambda x, y: x + y * COLS
-        if x < 0 or x > COLS - 1 or y < 0 or y > ROWS -1 :
-            return False
-        return grid_cells[find_index(x, y)]
-        
-    def check_neighbors(self, grid_cells):
-        neighbors = []
-        top = self.check_cell(self.x, self.y - 1, grid_cells)
-        right = self.check_cell(self.x + 1, self.y, grid_cells)
-        bottom = self.check_cell(self.x, self.y + 1, grid_cells)
-        left = self.check_cell(self.x - 1, self.y, grid_cells)
-        
-        if top and not top.visited:
-            neighbors.append(top)
-        if right and not right.visited:
-            neighbors.append(right)
-        if bottom and not bottom.visited:
-            neighbors.append(bottom)
-        if left and not left.visited:
-            neighbors.append(left)
-            
-        return choice(neighbors) if neighbors else False
-    
         
 if __name__ == "__main__":
     game = Game()
     game.run()
-    game.save_to_csv()
-    game.print_maze()
